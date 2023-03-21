43574534788fd3e39c33c0585f8f0152889fab01
Keziah Gertrude Rosales <krosales.intern.integratr@outlook.com>
2023-03-21 11:31:53 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<?php
+
+$course1 = [
+    'BSIT',
+    'BSBA',
+    'HMO',
+    'NC2',
+    'ENGiNEERING',
+    'NURSE',
+    'ARCHITECT',
+    'TOURISM'
+];
+
+$course2 = [
+    'HMO',
+    'ENGiNEERING',
+    'NURSE'
+];
+
+ $result = array_diff($course1, $course2); 
+
+?>
